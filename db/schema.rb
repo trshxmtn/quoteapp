da50deeb4dba0d039b8bcd685bae365fd5c8fa4b
Hironori Mitani <sangudadian@gmail.com>
2019-07-01 20:48:12 +0900

    Make comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_231336) do
+ActiveRecord::Schema.define(version: 2019_07_01_114522) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "rhetoric_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "picks", force: :cascade do |t|
     t.integer "user_id"
