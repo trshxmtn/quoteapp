95a08fb63769bcc0336ad57ab3af269bb1c489ea
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2019-07-14 02:38:31 +0900

    setup amazons3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = 's3-quote'
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: ENV['AWS_REGION'],
+        path_style: true
+    }
+  else
+    config.storage :clear_log_file
+    config.enable_processing = false if Rails.env.test?
+  end
+end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
