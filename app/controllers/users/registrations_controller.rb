6850bc1e002266e8773bb59931c88b435471b20c
Hironori Mitanie <sangudadian@gmail.com>
2019-07-20 07:22:48 +0900

    Update follow

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,14 +10,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def following
     @title = "フォロー"
-    @user  = User.friendly.find(params[:id])
+    @user  = User.find(params[:id])
     @users = @user.following
     render 'show_follow'
   end
 
   def followers
     @title = "フォロワー"
-    @user  = User.friendly.find(params[:id])
+    @user  = User.find(params[:id])
     @users = @user.followers
     render 'show_follow'
   end
