f9da9b3e171efd2c874388469089c182252b881f
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2019-07-20 19:23:22 +0900

    Quote画像を本番ではS3にアップロード

--- app/helpers/rhetorics_helper.rb
+++ app/helpers/rhetorics_helper.rb
@@ -7,7 +7,7 @@ module RhetoricsHelper
   GRAVITY = 'center'.freeze
   TEXT_POSITION = '0,0'.freeze
   FONT = './app/assets/fonts/幻ノにじみ明朝.otf'.freeze
-  FONT_SIZE = 22
+  FONT_SIZE = 44
   FONT_COLOR = "white"
   # 一行あたりの文字数
   INDENTION_COUNT = 12
@@ -16,9 +16,13 @@ module RhetoricsHelper
   class << self
     # 合成後のFileClassを生成
     def build(text)
+      # 元画像取得
       @image = MiniMagick::Image.open(BASE_IMAGE_PATH)
-      prepare_image(text)
+      # 文字列の整形
       text = prepare_text(text)
+      # 画像リサイズ
+      prepare_image(text)
+      # 画像に文字埋め込み
       configuration(text)
     end
 
@@ -38,11 +42,17 @@ module RhetoricsHelper
     # 設定関連の値を代入
     def configuration(text)
       @image.combine_options do |config|
+        # フォント
         config.font FONT
+        # 文字色
         config.fill FONT_COLOR
+        # 文字描写位置
         config.gravity GRAVITY
+        # フォントサイズ
         config.pointsize FONT_SIZE
-        config.interline_spacing 6
+        # 行幅
+        config.interline_spacing 22
+        # 文字描写
         config.draw "text #{TEXT_POSITION} '#{text}'"
       end
     end
@@ -55,13 +65,13 @@ module RhetoricsHelper
     # 文字量に合わせて背景画像をリサイズ
     def prepare_image(text)
       # 文字描写前の画像の高さ
-      default_height = 88
+      default_height = 176
       # 行の数
       rows_count = text.length/12 + 1
       # 最終的な画像の高さ
-      image_height = default_height + rows_count * 28
+      image_height = default_height + rows_count * 56
       # 高さをリサイズ
-      @image.resize "350x#{image_height}!"
+      @image.resize "700x#{image_height}!"
     end
   end
 
