0cedb919db4947f2165f69801c97e6ce67caaf9c
Hironori Mitanie <sangudadian@gmail.com>
2019-07-27 23:18:25 +0900

    not complete

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,12 +1,12 @@
-// $(document).on('turbolinks:load', function (){
-    $(window).on('scroll', function() {
-        scrollHeight = $(document).height();
-        scrollPosition = $(window).height() + $(window).scrollTop();
-        if ( (scrollHeight - scrollPosition) / scrollHeight === 0) {
+$(document).on('turbolinks:load', function (){
+    // $(window).on('scroll', function() {
+    //     scrollHeight = $(document).height();
+    //     scrollPosition = $(window).height() + $(window).scrollTop();
+    //     if ( (scrollHeight - scrollPosition) / scrollHeight === 0) {
             $('.jscroll').jscroll({
-                contentSelector: '.main-rhetorics-index',
+                contentSelector: '.jscroll',
                 nextSelector: 'span.next:last a'
             });
-        }
-    });
-// });
+        // }
+    // });
+});
