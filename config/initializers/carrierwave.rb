8a85d6c712e226735c03b5d51e0ea92d522814cb
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2019-07-31 01:14:10 +0900

    delete staging.rb and add env variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,19 +7,7 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog/aws'
-    config.fog_directory = 's3quote'
-    config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: ENV['AWS_REGION'],
-        path_style: true
-    }
-    config.storage :fog
-  # stagingの場合はfog_directoryを変更
-  elsif Rails.env.staging?
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = 's3quote-stg'
+    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
     config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
