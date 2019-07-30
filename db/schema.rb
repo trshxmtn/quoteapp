020cc31ba141571be852f637c2cc2cda40ad1601
Banjoya Takumi <banjoyatakumi@gmail.com>
2019-07-30 17:37:20 +0900

    fix design

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2019_07_26_101944) do
   end
 
   create_table "rhetorics", force: :cascade do |t|
-    t.string "title"
     t.text "meigen"
     t.text "description"
     t.datetime "created_at", null: false
@@ -67,8 +66,6 @@ ActiveRecord::Schema.define(version: 2019_07_26_101944) do
     t.integer "user_id"
     t.string "ctype"
     t.binary "image"
-    t.string "username"
-    t.text "profile"
     t.integer "picks_count", default: 0, null: false
     t.text "speaker"
     t.index ["user_id"], name: "index_rhetorics_on_user_id"
