1b3ed0140746fad8f6514a5244a1c028aad4b926
Banjoya Takumi <banjoyatakumi@gmail.com>
2019-08-08 15:56:01 +0900

    masonry fix and profile margin fix

--- app/assets/javascripts/masonry.js
+++ app/assets/javascripts/masonry.js
@@ -1,13 +1,21 @@
 $(document).on('turbolinks:load', function() {
-$(window).load(function(){
 
-  $('#masonry-container').masonry({
-    itemSelector: '.mas-box',
-    columnWidth: 400,
-    transitionDuration: 0,
-    gutterWidth: 40,
-    "fitWidth": true
-  });
+  if (window.matchMedia( '(min-width: 767px)' ).matches) {
+    $('#masonry-container').masonry({
+      itemSelector: '.mas-box',
+      columnWidth: 400,
+      transitionDuration: 0,
+      gutterWidth: 40,
+      "fitWidth": true
+    });
+  } else {
+    $('#masonry-container').masonry({
+      itemSelector: '.mas-box',
+      columnWidth: 100%,
+      transitionDuration: 0,
+      gutterWidth: 40,
+      "fitWidth": true
+    });
+  };
 
-});
 });
