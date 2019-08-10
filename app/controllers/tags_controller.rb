0a8091951838e47d8de7df97f0b012c7658f6292
Hironori Mitanie <sangudadian@gmail.com>
2019-08-11 01:07:29 +0900

    make anime tag genre

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
 
   def index
-    @popular_tags = ActsAsTaggableOn::Tag.most_used(3) #最も使われている上位10個のタグの取得
+    @popular_tags = ActsAsTaggableOn::Tag.most_used(3) #最も使われている上位3(ほんとは10)個のタグの取得
 
     @top_hash = Hash.new
 
@@ -10,21 +10,22 @@ class TagsController < ApplicationController
       @top_hash.store(tag.name, popular_rhetoric.picture)
     end
 
-    # anime_names = ["ONE PIECE", "宇宙兄弟", "キングダム"]
-    #
-    # # , "ドラえもん", "BLEACH", "カイジ", "SLAM DUNK", "NARUTO", "はじめの一歩", "進撃の巨人", "闇金ウシジマくん", "王様達のヴァイキング", "コードギアス"
-    #
-    # anime_names.each do |name|
-    #   @anime_tags = ActsAsTaggableOn::Tag.find_by(name: name)
-    # end
-    #
-    # @anime_hash = Hash.new
-    #
-    # @anime_tags.each do |tag|
-    #   anime_rhetoric = Rhetoric.tagged_with("#{tag.name}").order("picks_count DESC").first
-    #   @anime_hash.store(tag.name, anime_rhetoric.picture)
-    # end
+  anime_names = ["ONE PIECE", "宇宙兄弟", "キングダム"]
 
+    # , "ドラえもん", "BLEACH", "カイジ", "SLAM DUNK", "NARUTO", "はじめの一歩", "進撃の巨人", "闇金ウシジマくん", "王様達のヴァイキング", "コードギアス"
+
+    @anime_tags = []
+
+    anime_names.each do |name|
+      @anime_tags.push(ActsAsTaggableOn::Tag.find_by(name: name))
+    end
+
+    @anime_hash = Hash.new
+
+    @anime_tags.each do |tag|
+      anime_rhetoric = Rhetoric.tagged_with("#{tag.name}").order("picks_count DESC").first
+      @anime_hash.store(tag.name, anime_rhetoric.picture)
+    end
 
   end
 
