c7360fdbad12e427e1a3641034a5201ce8ed9137
Hironori Mitanie <sangudadian@gmail.com>
2019-08-10 14:22:48 +0900

    まだ途中だけどタグ検索をやった

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 
   def index
     @popular_tags = ActsAsTaggableOn::Tag.most_used(1) #最も使われている上位10個のタグの取得
-    @popular_rhetoric = Rhetoric.tagged_with("#{@popular_tags.name}").maximum(:picks_count) #popular_tagがついている投稿の中で最もpickが多いものを取得
+    @popular_rhetorics = Rhetoric.tagged_with(@popular_tags.name) #popular_tagがついている投稿の中で最もpickが多いものを取得
   end
 
 end
