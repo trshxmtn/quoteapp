cb6271fb2ad2963f2a4f1b6436fd417bb11233f6
Hironori Mitanie <sangudadian@gmail.com>
2019-08-10 19:38:12 +0900

    popular-tag-link

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,19 @@
 class TagsController < ApplicationController
 
   def index
-    @popular_tags = ActsAsTaggableOn::Tag.most_used(1) #最も使われている上位10個のタグの取得
-    @popular_rhetorics = Rhetoric.tagged_with(@popular_tags.name) #popular_tagがついている投稿の中で最もpickが多いものを取得
+    @popular_tags = ActsAsTaggableOn::Tag.most_used(3) #最も使われている上位10個のタグの取得
+
+    @hash = Hash.new
+
+    @popular_tags.each do |tag|
+      popular_rhetoric = Rhetoric.tagged_with("#{tag.name}").order("picks_count DESC").first
+      @hash.store(tag.name, popular_rhetoric.picture)
+    end
+
+    # @movie = Rhetoric.tagged_with("映画")
+    movie = ["ONE PIECE", "宇宙兄弟", "キングダム", "ドラえもん", "BLEACH", "カイジ", "SLAM DUNK", "NARUTO", "はじめの一歩", "進撃の巨人", "闇金ウシジマくん", "王様達のヴァイキング", "コードギアス"]
+
+
   end
 
 end
