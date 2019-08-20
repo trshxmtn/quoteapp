676ea0244c36c3c37612b81fb2db695fe2d767cb
Banjoya Takumi <banjoyatakumi@gmail.com>
2019-08-20 16:11:48 +0900

    footer icon size/footer header hide/search bar hide/tagindex margin-bottom/like index fix

---
+++ app/assets/javascripts/jQuery_headerHide.js
@@ -0,0 +1,30 @@
+var startPos = 0,winScrollTop = 0;
+
+$(document).on('turbolinks:load', function(){
+
+    $(function () {
+        var ua = navigator.userAgent;
+        if ((ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0) && ua.indexOf('Mobile') > 0) {
+            // スマートフォン用処理
+            $(window).on('scroll',function(){
+                winScrollTop = $(this).scrollTop();
+                if (winScrollTop >= startPos) {
+                    if(winScrollTop >= 200){
+                        $('.header').addClass('hideUp');
+                        $('.footer').addClass('hideDown');
+                        $('.rhetoric-new-btn').addClass('hideDown');
+                    }
+                } else {
+                    $('.header').removeClass('hideUp');
+                    $('.footer').removeClass('hideDown');
+                    $('.rhetoric-new-btn').removeClass('hideDown');
+                }
+                startPos = winScrollTop;
+            });
+        
+        } else {
+            // PC用処理
+        }
+    })
+
+});
