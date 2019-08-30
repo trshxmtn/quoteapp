e10e291e577d0490d53b0838cabb6182073b6c59
Hironori Mitanie <sangudadian@gmail.com>
2019-08-30 11:03:19 +0900

    set NoImage.png

--- app/uploaders/rhetoric_picture_uploader.rb
+++ app/uploaders/rhetoric_picture_uploader.rb
@@ -17,6 +17,13 @@ class RhetoricPictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    #   # For Rails 3.1+ asset pipeline compatibility:
+    #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    "NoImage.png"
+    #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
