69543721c7308b93acbb7dd0e4812322f4d03fbd
Banjoya Takumi <banjoyatakumi@gmail.com>
2020-01-20 18:44:26 +0900

    remove change to fix tab bug

--- app/assets/javascripts/profiletab.js
+++ app/assets/javascripts/profiletab.js
@@ -2,29 +2,11 @@ $(document).on('turbolinks:load', function() {
     $('.profile-tab-contents > div').hide();
 
     $('.profile-tab').click(function () {
-        var $container = $('#masonry-container');
-        $container.imagesLoaded(function(){
-          if (window.matchMedia( '(max-width: 767px)' ).matches) {
-            $('#masonry-container').masonry({
-              itemSelector: '.masonry-item',
-              transitionDuration: 0,
-              gutterWidth: 40,
-              "fitWidth": true
-            });
-          } else {
-            $('#masonry-container').masonry({
-              itemSelector: '.masonry-item',
-              columnWidth: 400,
-              transitionDuration: 0,
-              gutterWidth: 40,
-              "fitWidth": true
-            });
-          };
-        });
-
+        
         $('.profile-tab-contents > div').hide().filter(this.hash).fadeIn();
 
         $('.profile-tab').removeClass('active');
+
         $(this).addClass('active');
 
         return false;
